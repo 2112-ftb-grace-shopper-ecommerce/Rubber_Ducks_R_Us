1d8e99a81e226cd251049fa611d3867f44c44e09
Jenna <itcodegirl@gmail.com>
2022-05-11 19:41:51 -0500

    Jenna File Updates

---
+++ build/precache-manifest.70a09bea120f1599db1fd8f72b5c5502.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7bd23d5f4e9c3b4c71e99d8ea5e81b7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2313b088cb0eb2a2a9b7",
+    "url": "/static/css/main.5a6b043f.chunk.css"
+  },
+  {
+    "revision": "5e0b0e50504c55111401",
+    "url": "/static/js/2.ab0155c1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ab0155c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2313b088cb0eb2a2a9b7",
+    "url": "/static/js/main.ef31ff0f.chunk.js"
+  },
+  {
+    "revision": "7e23c3461cd3b1e0abd6",
+    "url": "/static/js/runtime-main.da5551cf.js"
+  }
+]);
