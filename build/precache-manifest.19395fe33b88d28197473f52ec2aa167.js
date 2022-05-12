dc59c33d456502edfbc8686b6e043bed9ea02a77
BBee8 <bbee8happy@gmail.com>
2022-05-11 21:11:15 -0500

    ok

---
+++ build/precache-manifest.19395fe33b88d28197473f52ec2aa167.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49dc1960f0d81a433379a218b3e60838",
+    "url": "/index.html"
+  },
+  {
+    "revision": "072beeeb0f03785990a6",
+    "url": "/static/css/main.9b8c1b41.chunk.css"
+  },
+  {
+    "revision": "5e0b0e50504c55111401",
+    "url": "/static/js/2.ab0155c1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ab0155c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "072beeeb0f03785990a6",
+    "url": "/static/js/main.425836bc.chunk.js"
+  },
+  {
+    "revision": "7e23c3461cd3b1e0abd6",
+    "url": "/static/js/runtime-main.da5551cf.js"
+  }
+]);
