e7ed78640a668e4ad58c53bc67d28d528b1aea99
itcodegirl@gmail.com <88835851+itcodegirl@users.noreply.github.com>
2022-05-24 22:04:15 -0500

    Update LoginModal.js

--- src/components/LoginModal.js
+++ src/components/LoginModal.js
@@ -1,15 +1,11 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import { Form, Input, Button, Modal } from "semantic-ui-react";
 
-const LoginModal = ({ loginShow, loginSetShow, setLogin, setUser }) => {
+const Login = ({ loginShow, setLogin, setUser }) => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
 
-  const handleClose = () => {
-    loginSetShow(false);
-  };
-
+ 
   const loginUser = () => {
     console.log("In login user!");
 
@@ -40,92 +36,5 @@ const LoginModal = ({ loginShow, loginSetShow, setLogin, setUser }) => {
         console.error("Error logging-in user!", error);
       });
   };
-  const clearForm = () => {
-    setUsername("");
-    setPassword("");
-  };
 
-  return (
-    <div>
-      <Modal open={loginShow} size="mini">
-        <Modal.Header
-          className="login-user-header"
-          style={{
-            backgroundColor: "olivedrab",
-            color: "white",
-            borderBottom: "2px solid black",
-          }}
-        >
-          User Login
-        </Modal.Header>
-        <Modal.Content
-          style={{
-            backgroundColor: "lightgrey",
-          }}
-        >
-          <Form>
-            <Form.Field required>
-              <label>Username:</label>
-              <Input
-                type="text"
-                placeholder="Username"
-                style={{
-                  border: "1px solid black",
-                  borderRadius: "5px",
-                }}
-                onChange={(event) => setUsername(event.target.value)}
-                value={username}
-              />
-            </Form.Field>
-            <Form.Field required>
-              <label>Password:</label>
-              <Input
-                type="password"
-                placeholder="Enter Password"
-                style={{
-                  border: "1px solid black",
-                  borderRadius: "5px",
-                }}
-                onChange={(event) => setPassword(event.target.value)}
-                value={password}
-              />
-            </Form.Field>
-          </Form>
-        </Modal.Content>
-        <Modal.Actions
-          style={{
-            backgroundColor: "darkgrey",
-            color: "white",
-            borderTop: "2px solid black",
-          }}
-        >
-          <Button
-            negative
-            style={{
-              boxShadow: "3px 3px 5px black",
-            }}
-            onClick={handleClose}
-          >
-            Cancel
-          </Button>
-          <Button
-            positive
-            icon="checkmark"
-            labelPosition="right"
-            content="Submit"
-            style={{
-              backgroundColor: "olivedrab",
-              boxShadow: "3px 3px 5px black",
-            }}
-            onClick={(event) => {
-              loginUser();
-              handleClose();
-              clearForm();
-            }}
-          />
-        </Modal.Actions>
-      </Modal>
-    </div>
-  );
-};
-export default LoginModal;
+export default Login;
